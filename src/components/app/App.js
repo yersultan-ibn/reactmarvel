48a44ac6377447cc60a52d66e4fb4e5ecbba282e
Ersultan <example@gmail.com>
2022-09-30 12:25:00 +0300

    152. Практика с жизненным циклом, componentDidUpdate

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,39 @@
+import { Component } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 
-import decoration from '../../resources/img/vision.png';
+import decoration from "../../resources/img/vision.png";
 
-const App = () => {
+class App extends Component {
+  state = {
+    showRandomChar: true,
+  };
+  toggleRandomChar = () => {
+    this.setState((state) => {
+      return {
+        showRandomChar: !state.showRandomChar,
+      };
+    });
+  };
+  render() {
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
-    )
+      <div className="app">
+        <AppHeader />
+        <main>
+          {this.state.showRandomChar ? <RandomChar /> : null}
+          <button onClick={this.toggleRandomChar}>Click me</button>
+    
+          <div className="char__content">
+            <CharList />
+            <CharInfo />
+          </div>
+          <img className="bg-decoration" src={decoration} alt="vision" />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,38 +2,37 @@ class MarvelService {
   _apiBase = "https://gateway.marvel.com:443/v1/public/";
   _apiKey = "apikey=c210cc7a83a2a69db5e35763811d8a76";
 
+ 
   getResource = async (url) => {
     let res = await fetch(url);
+
     if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
     }
 
     return await res.json();
-  };
+}
 
-  getAllCharacters = async () => {
-    const res = await this.getResource(
-      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
-    );
+getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
     return res.data.results.map(this._transformCharacter);
-  };
+}
 
-  getCharacter = async (id) => {
-    const res = await this.getResource(
-      `${this._apiBase}characters/${id}?${this._apiKey}`
-    );
+getCharacter = async (id) => {
+    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
     return this._transformCharacter(res.data.results[0]);
-  };
+}
 
-  _transformCharacter = (char) => {
+_transformCharacter = (char) => {
     return {
-      name: char.name,
-      description: char.description,
-      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
-      homepage: char.urls[0].url,
-      wiki: char.urls[1].url,
-    };
-  };
+        id: char.id,
+        name: char.name,
+        description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+        thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+        homepage: char.urls[0].url,
+        wiki: char.urls[1].url
+    }
+}
 }
 
 export default MarvelService;
