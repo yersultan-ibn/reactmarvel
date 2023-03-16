96b0f580c0213318e9a36a53dfb33ef2ef1e165e
Ersultan <example@gmail.com>
2023-03-16 14:51:50 +0300

    add charSearchForm

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,79 +1,101 @@
-import "./charInfo.scss";
-import { useEffect, useState } from "react";
-import Spinner from "../spinner/Spinner";
-import ErrorMessage from "../errorMessage/ErrorMessage";
-import Skeleton from "../skeleton/Skeleton";
-import useMarvelService from "../../services/MarvelService";
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import useMarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import Skeleton from '../skeleton/Skeleton';
+
+import './charInfo.scss';
 
 const CharInfo = (props) => {
-  const [char, setChar] = useState(null);
 
-  const { error, loading, getCharacter } = useMarvelService();
+    const [char, setChar] = useState(null);
 
-  useEffect(() => {
-    updateChar();
-  }, [props.charId]);
+    const {loading, error, getCharacter, clearError} = useMarvelService();
 
-  const updateChar = () => {
-    const charId = props.charId;
-    if (!charId) {
-      return;
+    useEffect(() => {
+        updateChar()
+    }, [props.charId])
+
+    const updateChar = () => {
+        const {charId} = props;
+        if (!charId) {
+            return;
+        }
+
+        clearError();
+        getCharacter(charId)
+            .then(onCharLoaded)
     }
 
-    getCharacter(charId).then(onCharLoaded);
-  };
+    const onCharLoaded = (char) => {
+        setChar(char);
+    }
 
-  const onCharLoaded = (char) => {
-    setChar(char);
-  };
+    const skeleton = char || loading || error ? null : <Skeleton/>;
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = !(loading || error || !char) ? <View char={char}/> : null;
 
-  const skeleten = char || loading || error ? null : <Skeleton />;
-  const spinner = loading ? <Spinner /> : null;
-  const errorMessage = error ? <ErrorMessage /> : null;
-  const content = !(loading || error || !char) ? <View char={char} /> : null;
-  return (
-    <div className="char__info">
-      {skeleten}
-      {spinner}
-      {errorMessage}
-      {content}
-    </div>
-  );
-};
-
-const View = ({ char }) => {
-  const { name, description, thumbnail, homepage, wiki, comics } = char;
-  return (
-    <>
-      <div className="char__basics">
-        <img src={thumbnail} alt="abyss" />
-        <div>
-          <div className="char__info-name">{name}</div>
-          <div className="char__btns">
-            <a href={homepage} className="button button__main">
-              <div className="inner">homepage</div>
-            </a>
-            <a href={wiki} className="button button__secondary">
-              <div className="inner">Wiki</div>
-            </a>
-          </div>
+    return (
+        <div className="char__info">
+            {skeleton}
+            {errorMessage}
+            {spinner}
+            {content}
         </div>
-      </div>
-      <div className="char__descr">{description}</div>
-      <div className="char__comics">Comics:</div>
+    )
+}
 
-      <ul className="char__comics-list">
-        {comics.map((item, i) => {
-          if (item.length > 1) return;
-          return (
-            <li key={i} className="char__comics-item">
-              {item.name}
-            </li>
-          );
-        })}
-      </ul>
-    </>
-  );
-};
+const View = ({char}) => {
+    const {name, description, thumbnail, homepage, wiki, comics} = char;
+
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
+
+    return (
+        <>
+            <div className="char__basics">
+                <img src={thumbnail} alt={name} style={imgStyle}/>
+                <div>
+                    <div className="char__info-name">{name}</div>
+                    <div className="char__btns">
+                        <a href={homepage} className="button button__main">
+                            <div className="inner">homepage</div>
+                        </a>
+                        <a href={wiki} className="button button__secondary">
+                            <div className="inner">Wiki</div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="char__descr">
+                {description}
+            </div>
+            <div className="char__comics">Comics:</div>
+            <ul className="char__comics-list">
+                {comics.length > 0 ? null : 'There is no comics with this character'}
+                {
+                    comics.map((item, i) => {
+                        // eslint-disable-next-line
+                        if (i > 9) return;
+                        return (
+                            <li key={i} className="char__comics-item">
+                                {item.name}
+                            </li>
+                        )
+                    })
+                }                
+            </ul>
+        </>
+    )
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
+}
 
 export default CharInfo;
--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,37 +1,32 @@
 import { useState, useCallback } from "react";
 
 export const useHttp = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-  const request = useCallback(
-    async (
-      url,
-      method = "GET",
-      body = null,
-      headers = { "Content-Type": "application/json" }
-    ) => {
-      setLoading(true);
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
 
-      try {
-        const response = await fetch(url, { method, body, headers });
+        setLoading(true);
 
-        if (!response.ok) {
-          throw new Error(`Could not fetch url: ${url} status ${url.status}`);
+        try {
+            const response = await fetch(url, {method, body, headers});
+
+            if (!response.ok) {
+                throw new Error(`Could not fetch ${url}, status: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            setLoading(false);
+            return data;
+        } catch(e) {
+            setLoading(false);
+            setError(e.message);
+            throw e;
         }
+    }, []);
 
-        setLoading(false);
-        const data = await response.json();
-        return data;
-      } catch (e) {
-        setLoading(false);
-        setError(e.message);
-        throw e;
-      }
-    },
-    []
-  );
-  const clearError = useCallback(() => setError(null), []);
+    const clearError = useCallback(() => setError(null), []);
 
-  return { request, loading, error, clearError };
-};
+    return {loading, request, error, clearError}
+}
